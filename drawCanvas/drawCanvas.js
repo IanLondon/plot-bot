5afdb6c602ef8f9d1b7c42ac86810f50a091b308
hail-seitan <ianlondondu@gmail.com>
2015-04-23 21:23:57 -0400

    added lodash

--- drawCanvas/drawCanvas.js
+++ drawCanvas/drawCanvas.js
@@ -10,16 +10,20 @@ var Convert = {
 };
 
 
-var WallRobot = function() {
-  var MOTOR_DIST = Convert.ft_mm(12); // horizontal distance btw motors in mm
-  var MOTOR_HEIGHT = Convert.ft_mm(2); // height of motors above top of drawing area in mm
+var WallRobot = function(opts) {
+  //all working units are in MM! MM is "native".
+
+  // horizontal distance from motors to side of board in mm
+  this.MOTOR_SIDE_OFFSET = opts.MOTOR_SIDE_OFFSET;
+
+  // height of motors above top of drawing area in mm
+  this.MOTOR_HEIGHT = opts.MOTOR_HEIGHT;
+
+  this.DRAW_AREA_WIDTH = opts.DRAW_AREA_WIDTH;
+  this.DRAW_AREA_HEIGHT = opts.DRAW_AREA_HEIGHT;
 
   //steps for left and right stepper, from origin position.
-  var steps = [0,0];
-
-  //x,y are width, height of drawing area in MM.
-  //all working units are in MM! MM is "native".
-  var draw_area = new Victor (Convert.ft_mm(8),Convert.ft_mm(4));
+  this.steps = [0,0];
 
   // Right now displacement vectors are named like l_p meaning point L to point P.
   // CONSTANT vectors are uppercase and variable vectors are lowercase.
@@ -30,16 +34,26 @@ var WallRobot = function() {
   // O   Origin of drawing area, where (x,y) = (0,0)
   // P   Marker Position
 
-  var o_p = new Victor(0, 0);
-  var L_R = new Victor(0, MOTOR_DIST)); //horizontal dist btw steppers
+  this.o_p = new Victor(0, 0);
+  this.L_R = new Victor(0, this.MOTOR_SIDE_OFFSET*2 + this.DRAW_AREA_WIDTH); //horizontal dist btw steppers
 
   // Assume that both steppers have the same y-axis displacement from
   // their side of the drawing area.
   // IE, they should be horizontally centered.
-  var L_O = new Victor( (L_R.x - draw_area.x)/2 , MOTOR_HEIGHT);
+  this.L_O = new Victor(this.MOTOR_SIDE_OFFSET, this.MOTOR_HEIGHT);
 
   //The marker starts at (0,0) so L_O = l_p
-  var l_p = L_O.clone();
+  this.l_p = this.L_O.clone();
+
+  this.sanityCheck = function () {
+    _.forEach([MOTOR_SIDE_OFFSET, MOTOR_HEIGHT, DRAW_AREA_WIDTH, DRAW_AREA_HEIGHT], function(param) {
+      if (! this[param] > 0) {
+        throw new Error(
+          param + " must be > 0."
+        );
+      }
+    });
+  }
 };
 
 
@@ -62,3 +76,11 @@ function randomNoise() {
   }
   context.putImageData(imageData, 0,0);
 }
+
+// start!!!
+var robot = new WallRobot({
+  MOTOR_SIDE_OFFSET: Convert.ft_mm(2),
+  MOTOR_HEIGHT: Convert.ft_mm(2),
+  DRAW_AREA_WIDTH: Convert.ft_mm(8),
+  DRAW_AREA_HEIGHT: Convert.ft_mm(4)
+});
