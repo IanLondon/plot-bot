0ddfbc9e156ca8fd4cad1ae59fbcd8d93439649d
hail-seitan <ianlondondu@gmail.com>
2015-05-02 00:52:39 -0400

    the substepping market drag worksgit log

--- drawCanvas/robotDrawModel.js
+++ drawCanvas/robotDrawModel.js
@@ -13,36 +13,69 @@ var stepDelta = [0,0];
 
 function isInt(value) {
   // from krisk on http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
-  return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
+  return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value));
 }
 
 function step(stepsLeft, stepsRight) {
   //steps is positive -> extend string
   //steps is negative -> retract string
-  var drawResolution = 5;
+  var substepResolution = 2;
+  var deltaDiff = [];
 
-  if (!isInt(stepsLeft) && !isInt(stepsRight)) {
+  if (!isInt(stepsLeft) || !isInt(stepsRight)) {
     throw new Error("Steps must be an integer! Got " + stepsLeft + ", " + stepsRight);
   }
+
+  var prevStepDelta = stepDelta.slice();
 
   //Update stepDelta with new position
   stepDelta[0] += stepsLeft;
   stepDelta[1] += stepsRight;
 
+  var newStepDelta = stepDelta.slice();
+
   // Draw the sampled points across the displacement
+  var coords;
+  var substeps = [];
+
   context.strokeStyle = "rgba(255,0,0,0.5)";
-  for (var i = 0; i < drawResolution; i++) {
-    // TODO !!
+
+  for (var diff_i = 0; diff_i < 2; diff_i++) {
+    deltaDiff[diff_i] = newStepDelta[diff_i] - prevStepDelta[diff_i];
+  }
+  var biggestDiff = Math.abs(deltaDiff[0]) > Math.abs(deltaDiff[1]) ? Math.abs(deltaDiff[0]) : Math.abs(deltaDiff[1]);
+  var totalSubsteps = biggestDiff*substepResolution;
+
+  //The sign of the displacement.
+  var stepDirections = _.map(deltaDiff,function(val){
+    if (val > 0) {
+      return 1;
+    } else if (val < 0) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  for (var substep_i = 0; substep_i < totalSubsteps; substep_i++) {
     // this should take the step directions as input, and draw a certain number
     // of points between those directions.
-    // throw new Error("DRAW THIS OUT!!");
-//    drawCircle(Math.round(x), Math.round(y), 1, 0, 2*Math.PI);
+
+    console.log("stepDirections: " + stepDirections);
+
+    tempStepDelta = _.map(prevStepDelta, function(val, index) {
+      return val + stepDirections[index]*substep_i/substepResolution;
+    });
+
+    console.log("tempStepDelta: " + tempStepDelta);
+
+    coords = getCartesian(tempStepDelta);
+    drawCircle(coords.x, coords.y, 1, 0, 2*Math.PI);
   }
 
   updateCursor();
 }
 
-function getCartesian(someStepDelta, roundXY) {
+function getCartesian(someStepDelta) {
   //clone the array, it's not necessary - but just to be safe
   someStepDelta = someStepDelta.slice();
 
@@ -51,14 +84,8 @@ function getCartesian(someStepDelta, roundXY) {
   var s_r = someStepDelta[1]*STEP_LEN + WIDTH;
 
   //cartesian coords
-  var x = (Math.pow(s_l, 2) - Math.pow(s_r, 2)
-    + Math.pow(WIDTH, 2) ) / (2 * WIDTH);
+  var x = (Math.pow(s_l, 2) - Math.pow(s_r, 2) + Math.pow(WIDTH, 2) ) / (2 * WIDTH);
   var y = Math.sqrt( Math.abs( Math.pow(s_l, 2) - Math.pow(x,2) ));
-
-  if (roundXY===true) {
-    x = Math.round(x);
-    y = Math.round(y);
-  }
 
   return {x:x, y:y, s_l:s_l, s_r:s_r};
 }
@@ -73,10 +100,10 @@ function updateCursor() {
   // bug: something weird happens when you pull
   // the line enough to "stretch" it
 
-  var coords = getCartesian(stepDelta)
+  var coords = getCartesian(stepDelta);
 
   // draw the cursor position
-  context.strokeStyle = "rgba(0, 0, 0, 0.25)";
+  context.strokeStyle = "rgba(0, 0, 0, 0.10)";
   drawCircle(coords.x, coords.y, 3, 0, 2*Math.PI);
 }
 
@@ -87,8 +114,8 @@ function drawCircle(x,y,radius,startAngle,endAngle,counterClockwise) {
   context.stroke();
 }
 
-// Draw the possible coordinates.
-for (var i = 0; i < 800; i+=STEP_LEN) {
+// Draw the possible coordinates in a bipolar "grid".
+for (var i = 0; i < 1000; i+=STEP_LEN) {
   context.strokeStyle = "rgba(255, 0, 0, 0.10)";
   drawCircle(0, 0, i, 0, 2*Math.PI, false);
 
@@ -120,4 +147,4 @@ document.body.onkeydown = function(event){
       step(0,1);
     }
 
-}
+};
