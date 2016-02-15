7d70e8245ec56c5cd9d23fb935bdbbe54eb3d3d2
IanLondon <ianlondondu@gmail.com>
2016-02-15 01:15:31 -0500

    minor

--- drawCanvas/fontTest.js
+++ drawCanvas/fontTest.js
@@ -9,14 +9,14 @@ opentype.load('fonts/candy_shop/Candy\ Shop\ Black.ttf', function(err, font) {
         // Construct a Path object containing the letter shapes of the given text.
         // The other parameters are x, y and fontSize.
         // Note that y is the position of the baseline.
-        var path = font.getPath('hi there', 100, 250, 92);
+        var path = font.getPath('hai', 100, 350, 200);
         // If you just want to draw the text you can also use font.draw(ctx, text, x, y, fontSize).
 
         // DEBUG: show the stroke of the text
-        show_stroke = false;
+        show_stroke = true;
         if (show_stroke) {
             path.fill = null;
-            path.stroke = 'black';
+            path.stroke = 'rgba(0,255,0,0.25)';
             path.draw(ctx);
         }
         cmds = path.commands
@@ -24,9 +24,10 @@ opentype.load('fonts/candy_shop/Candy\ Shop\ Black.ttf', function(err, font) {
         // path.commands has x y coords, all except the last
         for (i = 0; i < cmds.length; i++) {
             if (cmds[i].type != 'Z') {
-                moveRobotTo(getBipolarCoords(cmds[i].x,cmds[i].y));
+                // drawStraightLine is probably better, test IRL XXX
+                drawStraightLine(getBipolarCoords(cmds[i].x,cmds[i].y));
+                // moveRobotTo(getBipolarCoords(cmds[i].x,cmds[i].y));
             }
         }
-
     }
 });
